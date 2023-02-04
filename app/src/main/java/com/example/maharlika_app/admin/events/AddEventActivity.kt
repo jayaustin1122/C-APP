5b7506dda2f54d30010aea1b774e738358d4aed8
unknown <bobisjayaustin5@gmail.com>
2023-02-04 10:39:01 +0800

    Testing store notif in every users accounts

--- app/src/main/java/com/example/maharlika_app/admin/events/AddEventActivity.kt
+++ app/src/main/java/com/example/maharlika_app/admin/events/AddEventActivity.kt
@@ -12,14 +12,19 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.maharlika.ui.admin.events.ModelEvent
 import com.example.maharlika_app.admin.AdminHolderActivity
+import com.example.maharlika_app.admin.news.ModelNews
 import com.example.maharlika_app.auth.LoginActivity
 import com.example.maharlika_app.auth.SignUpActivity
 import com.example.maharlika_app.databinding.ActivityAddEventBinding
+import com.example.maharlika_app.model.UserModel
 import com.example.maharlika_app.notifications.NotificationData
 import com.example.maharlika_app.notifications.PushNotification
 import com.example.maharlika_app.notifications.RetrifitInstance
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.storage.FirebaseStorage
@@ -30,6 +35,7 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 const val TOPIC = "/topics/myTopic"
 
@@ -40,6 +46,7 @@ class AddEventActivity : AppCompatActivity() {
     private lateinit var storage : FirebaseStorage
     private lateinit var database : FirebaseDatabase
     private lateinit var selectedImage : Uri
+    private lateinit var accArrayList : ArrayList<UserModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddEventBinding.inflate(layoutInflater)
@@ -122,47 +129,58 @@ class AddEventActivity : AppCompatActivity() {
     private fun uploadInfo(imgUrl: String) {
         progressDialog.setMessage("Saving Event...")
         progressDialog.show()
-        title = binding.etTitleEvents.text.toString()
-        description = binding.etDesciptionEvents.text.toString().trim()
-        val currentDate = getCurrentDate()
-        val timestamp = System.currentTimeMillis()
-        val currentTime = getCurrentTime()
-        val uid = auth.uid
-        val hashMap : HashMap<String,Any?> = HashMap()
-        hashMap["uid"] = uid
-        hashMap["eventsTitle"] = title
-        hashMap["eventsDescription"] = description
-        hashMap["image"] = imgUrl
-        hashMap["currentDate"] = currentDate
-        hashMap["currentTime"] = currentTime
-        hashMap["id"] = "$timestamp"
 
-        val hashMap2 : HashMap<String,Any?> = HashMap()
-        hashMap2["uidNotif"] = uid
-        hashMap2["NotifTitle"] = title
-        hashMap2["NotifDescription"] = description
-        hashMap2["imageNotif"] = imgUrl
-        hashMap2["currentDateNotif"] = currentDate
-        hashMap2["currentTimeNotif"] = currentTime
-        hashMap2["id"] = "$timestamp"
+        accArrayList = ArrayList()
 
-        database.getReference("Notifications")
-            .child(timestamp.toString())
-            .setValue(hashMap2)
-        database.getReference("events")
-            .child(timestamp.toString())
-            .setValue(hashMap)
-            .addOnCompleteListener{
-                if (it.isSuccessful){
-                    progressDialog.dismiss()
-                    val intent = Intent(this, AdminHolderActivity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this,"Event Added", Toast.LENGTH_SHORT).show()
-                }
-                else{
-                    Toast.makeText(this,it.exception!!.message, Toast.LENGTH_SHORT).show()
+        val dbRef = FirebaseDatabase.getInstance().getReference("Users")
+        dbRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                accArrayList.clear()
+                for (data in snapshot.children){
+                    val model = data.getValue(UserModel::class.java)
+                    title = binding.etTitleEvents.text.toString()
+                    description = binding.etDesciptionEvents.text.toString().trim()
+                    val currentDate = getCurrentDate()
+                    val timestamp = System.currentTimeMillis()
+                    val currentTime = getCurrentTime()
+                    val uid = model?.uid
+                    val hashMap : HashMap<String,Any?> = HashMap()
+                    hashMap["uid"] = uid
+                    hashMap["eventsTitle"] = title
+                    hashMap["eventsDescription"] = description
+                    hashMap["image"] = imgUrl
+                    hashMap["currentDate"] = currentDate
+                    hashMap["currentTime"] = currentTime
+                    hashMap["id"] = "$timestamp"
+
+
+                    database.getReference("Users")
+                        .child(model?.uid!!)
+                        .child("eventNotifications")
+                        .child(timestamp.toString())
+                        .setValue(hashMap)
+                    database.getReference("events")
+                        .child(timestamp.toString())
+                        .setValue(hashMap)
+                        .addOnCompleteListener{
+                            if (it.isSuccessful){
+                                progressDialog.dismiss()
+                                    startActivity(Intent(this@AddEventActivity,AdminHolderActivity::class.java))
+                            }
+                            else{
+
+                            }
+                        }
                 }
             }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
     }
 
 
